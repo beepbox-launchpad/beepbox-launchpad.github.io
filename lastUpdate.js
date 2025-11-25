fadf6113728c451e0356c5eca63759b5d9707996
slarmoo <matthew.s.medford@gmail.com>
2025-11-25 13:43:56 -0500

    jukebox update

--- lastUpdate.js
+++ lastUpdate.js
@@ -33,7 +33,7 @@ export function calculateNewUpdate(mod) {
             return jummBoxPN ? "unset" : "none";
         }
         case "JukeBox": {
-            var jukeBoxPN = compareDates(23, 11, 2025);
+            var jukeBoxPN = compareDates(2, 12, 2025);
             return jukeBoxPN ? "unset" : "none";
         }
         case "EdoBox": {
